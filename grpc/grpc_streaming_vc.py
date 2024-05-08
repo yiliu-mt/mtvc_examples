9fc8e894f6474e4cb72e328f8a8740a0a32a8dbb
yiliu-mt <yi.liu@mthreads.com>
2024-05-08 16:35:14 +0800

    first commit

---
+++ grpc/grpc_streaming_vc.py
@@ -0,0 +1,114 @@
+import argparse
+import time
+import grpc
+import wave
+import numpy as np
+from scipy.io.wavfile import write
+from proto import service_pb2, service_pb2_grpc
+
+
+input_sample_rate = 16000
+output_sample_rate = 48000
+
+
+def bytes_to_np_array(audio_bytes, encoding, channels):
+    '''Convert audio bytes to numpy array
+
+    Args:
+      encoding: enum options: pcm
+      channels: the num of channels
+    '''
+    if encoding == "pcm":
+        np_array = np.frombuffer(audio_bytes, dtype=np.int16)
+    else:
+        raise NotImplemented(f"Unsupported encoding : {encoding}")
+    return np_array.reshape(-1, channels)
+
+
+def load_wav_file(filename):
+    """
+    Load a WAV file and return its raw PCM data, sample rate, number of channels, and sample width.
+    """
+    with wave.open(filename, 'rb') as wf:
+        sample_rate = wf.getframerate()
+        channels = wf.getnchannels()
+        sample_width = wf.getsampwidth()
+        audio_data = wf.readframes(wf.getnframes())
+    
+    return audio_data, sample_rate, channels, sample_width
+
+
+def request_generator(target_voice, input_audio_path, chunk_size, simulate_delay):
+    '''generator to send the gRPC request
+    '''
+    audio_data, sample_rate, channels, sample_width = load_wav_file(input_audio_path)
+    assert sample_rate == input_sample_rate, f"The sample rate {sample_rate} is not supported"
+    assert channels == 1, f"The channels {channels} is not supported"
+    assert sample_width == 2, f"The sample width {sample_width} is not supported"
+
+    # Send VoiceConversionConfig first
+    config = service_pb2.VoiceConversionConfig(
+        type="StartConversion",
+        payload=service_pb2.VoiceConversionConfig.Payload(
+            voice=target_voice,
+            input_info=service_pb2.VoiceConversionConfig.AudioInfo(
+                sample_rate=16000,
+                channels=1,
+                bits=16,
+                audio_encoding="pcm"
+            ),
+            output_info=service_pb2.VoiceConversionConfig.AudioInfo(
+                sample_rate=48000,
+                channels=1,
+                bits=16,
+                audio_encoding="pcm"
+            )
+        )
+    )
+    print("sending config...")
+    yield service_pb2.StreamingVCRequest(config=config)
+
+    # Stream audio data
+    bytes_per_chunk = int(sample_rate * channels * sample_width * chunk_size / 1000.)
+    for i in range(0, len(audio_data), bytes_per_chunk):
+        # type = service_pb2.NORMAL_CHUNK if i+bytes_per_chunk < len(audio_data) else service_pb2.LAST_CHUNK
+        chunk = audio_data[i:i + bytes_per_chunk]
+        if simulate_delay:
+            time.sleep(len(chunk) / sample_rate / sample_width)
+        print("sending audio data...")
+        yield service_pb2.StreamingVCRequest(audio_data=chunk)
+
+    config = service_pb2.VoiceConversionConfig(type="StopConversion")
+    yield service_pb2.StreamingVCRequest(config=config)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hostname', type=str, default='127.0.0.1', help='hostname')
+    parser.add_argument('--port', type=int, default=50051, help='port')
+    parser.add_argument('--voice', type=str, required=True, help='the target voice')
+    parser.add_argument("--simulate_delay", type=lambda x: (str(x).lower() == 'true'), default=False, help="Whether to delay between packets")
+    parser.add_argument('--chunk_size', type=int, default=1000, help='the chunk size for each package (in ms)')
+    parser.add_argument("--input_wav", type=str, required=True, help='the path of the input WAV file')
+    parser.add_argument("--output_wav", type=str, required=True, help='the path of the output WAV file')
+    args = parser.parse_args()
+
+    channel = grpc.insecure_channel('{}:{}'.format(args.hostname, args.port))
+    stub = service_pb2_grpc.VoiceConversionStub(channel)
+
+    responses = stub.StreamingVC(request_generator(args.voice, args.input_wav, args.chunk_size, args.simulate_delay))
+    audio_chunks = []
+    for response in responses:
+        if response.HasField("state"):
+            if response.state.type == "Error":
+                raise RuntimeError("Error occurs: {}".format(response.state.status_text))
+            elif response.state.type == "ConversionStarted":
+                print("Conversion started")
+            elif response.state.type == "ConversionCompleted":
+                print("Conversion completed")
+        else:
+            audio_chunk = bytes_to_np_array(response.audio_data, 'pcm', 1)[:, 0]
+            audio_chunks.append(audio_chunk)
+    
+    audio = np.concatenate(audio_chunks, axis=-1)
+    write(args.output_wav, output_sample_rate, audio)
---
+++ streaming_vc/python/streaming_demo.py
@@ -0,0 +1,154 @@
+import argparse
+import time
+import wave
+import json
+import logging
+import websocket
+import numpy as np
+from functools import partial
+from threading import Thread
+from scipy.io.wavfile import write
+
+
+DEFAULT_URL = 'ws://aidev.mthreads.com:45003/api/v2/vc/streaming_vc'
+
+INPUT_SR = 16000
+INPUT_CHANNELS = 1
+INPUT_BITS = 16
+INPUT_FORMAT = 'pcm'
+
+OUTPUT_SR = 48000
+OUTPUT_CHANNELS = 1
+OUTPUT_BITS=16
+OUTPUT_FORMAT = 'pcm'
+
+
+def bytes_to_np_array(audio_bytes, encoding, channels):
+    if encoding == "pcm":
+        np_array = np.frombuffer(audio_bytes, dtype=np.int16)
+    else:
+        raise NotImplemented(f"Unsupported encoding : {encoding}")
+    return np_array.reshape(-1, channels)
+
+
+class WsClient():
+    def __init__(self, args):
+        self.url = args.url
+        self.config_signal = {
+          "type": "StartConversion",
+          "payload":{
+            "voice": args.voice,
+            "input_info":{
+              "sample_rate": INPUT_SR,
+              "channels": INPUT_CHANNELS,
+              "bits": INPUT_BITS,
+              "audio_encoding": INPUT_FORMAT
+            },
+            "output_info":{
+              "sample_rate": OUTPUT_SR,
+              "channels": OUTPUT_CHANNELS,
+              "bits": OUTPUT_BITS,
+              "audio_encoding": OUTPUT_FORMAT
+            }
+          }
+        }
+        self.end_signal = {
+            "type": "StopConversion"
+        }
+        self.ws_header = None
+        if args.token is not None and len(args.token) > 0:
+            if args.mode == "cloud":
+                self.ws_header = {"Authorization": args.token}
+            elif args.mode == "local":
+                self.url = '{}?token={}'.format(self.url, args.token)
+
+        # Log设置
+        self.logger = logging.getLogger("RunLog")
+        self.logger.setLevel(logging.INFO)
+
+        self.audio_chunks = []
+
+    def on_open(self, ws, file_path):
+        def run(*args):
+            with wave.open(file_path, 'rb') as wf:
+                sample_rate = wf.getframerate()
+                channels = wf.getnchannels()
+                sample_width = wf.getsampwidth()
+                audio_data = wf.readframes(wf.getnframes())
+            assert sample_rate == INPUT_SR, f"The sample rate {sample_rate} is not supported"
+            assert channels == INPUT_CHANNELS, f"The channels {channels} is not supported"
+            assert sample_width == int(INPUT_BITS / 8), f"The sample width {sample_width} is not supported"
+
+            chunk_size = 1000
+            bytes_per_chunk = int(sample_rate * channels * sample_width * chunk_size / 1000.)
+
+            # 首先发送配置
+            ws.send(json.dumps(self.config_signal))
+
+            # 流式发送数据包
+            index = 0
+            while index < len(audio_data):
+                chunk = audio_data[index : index + bytes_per_chunk]
+                time.sleep(0.001)
+                # time.sleep(len(chunk) / sample_rate / channels / sample_width)
+                print("send chunk")
+                ws.send(chunk, 2)
+                index += bytes_per_chunk
+
+            # 发送尾包
+            print("send_stop")
+            ws.send(json.dumps(self.end_signal))
+        Thread(target=run).start()
+
+    def on_message(self, ws, message):
+        try:
+            json_data = json.loads(message)
+            if json_data['status'] != 1000:
+                print(json_data)
+                return
+            if json_data['type'] == 'ConversionStarted':
+                print("Start conversion")
+            elif json_data['type'] == 'ConversionCompleted':
+                print("Stop conversion")
+            elif json_data['type'] == 'Error':
+                print(json_data)
+        except:
+            print("receive")
+            audio_chunk = bytes_to_np_array(message, 'pcm', 1)[:, 0]
+            self.audio_chunks.append(audio_chunk)
+
+    def on_error(self, ws, error):
+        print("error: ", error)
+
+    def on_close(self, ws, close_status_code, close_msg):
+        ws.close()
+        print("### closed ###")
+
+    def send(self, file_path):
+        # start websocket-client
+        websocket.enableTrace(False)
+        ws = websocket.WebSocketApp(
+            self.url,
+            on_message=self.on_message,
+            on_error=self.on_error,
+            on_close=self.on_close,
+            header=self.ws_header,
+        )
+        ws.on_open = partial(self.on_open, file_path=file_path)
+        ws.run_forever()
+        return np.concatenate(self.audio_chunks, axis=-1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", default=DEFAULT_URL, type=str, help="The endpoint to connect to")
+    parser.add_argument("--mode", choices=["cloud", "local"], default="cloud", type=str, help="The authorization mode")
+    parser.add_argument("--token", default=None, type=str, help="The authorization token")
+    parser.add_argument('--voice', type=str, required=True, help='the target voice')
+    parser.add_argument("--input_file", type=str, required=True, help='the path of the input WAV file')
+    parser.add_argument("--output_file", type=str, required=True, help='the path of the output WAV file')
+    args = parser.parse_args()
+    ws_client = WsClient(args)
+    audio_data = ws_client.send(args.input_file)
+    write(args.output_file, OUTPUT_SR, audio_data)
+
